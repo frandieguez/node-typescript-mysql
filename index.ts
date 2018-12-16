006ff4d4312dd05357b323beab6ce12cd8f08285
Fran Dieguez <fran.dieguez@mabishu.com>
2018-12-16 17:48:20 +0100

    Add simple typecript class to define the server class

--- index.ts
+++ index.ts
@@ -1,1 +1,7 @@
-console.log('Codigo de typescript')
+import Server from './server/server';
+
+const server = Server.init(3000);
+
+server.start(() => {
+    console.log('Server running on port 3000');
+});
